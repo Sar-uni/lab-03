ffdc6459a6f0c0a116757eb65fbed0c36a6f0bbc
Sar-uni <stmoham1@ualberta.ca>
2025-09-18 16:38:19 -0600

    Update

---
+++ code/app/src/main/java/com/example/listycitylab3/EditCityFragment.java
@@ -0,0 +1,65 @@
+package com.example.listycitylab3;
+
+import static com.example.listycitylab3.R.*;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class EditCityFragment extends DialogFragment {
+
+    public interface EditCityDialogListener {
+        void updateCity(int position, City updatedCity);
+    }
+
+    private EditCityDialogListener listener;
+    private City cityToEdit;
+    private int position;
+
+    public EditCityFragment(City city, int position) {
+        this.cityToEdit = city;
+        this.position = position;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view =
+                getLayoutInflater().inflate(R.layout.fragment_edit_city, null);
+
+        EditText cityName = view.findViewById(R.id.city_text);
+        EditText provinceName = view.findViewById(R.id.province_text);
+
+        cityName.setText(cityToEdit.getName());
+        provinceName.setText(cityToEdit.getProvince());
+
+        builder.setView(view)
+                .setTitle("Edit City")
+                .setPositiveButton("Save", (dialog, which) -> {
+                    City updated = new City(
+                            cityName.getText().toString(),
+                            provinceName.getText().toString()
+                    );
+                    listener.updateCity(position, updated);
+                })
+                .setNegativeButton("Cancel", null);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        listener = (EditCityDialogListener) context;
+    }
+}
+
